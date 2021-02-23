cda93d24461960c42da1bd1575ab0701b2395cef
yujia.fu <838595821@qq.com>
2021-02-23 22:46:06 +0100

    删除无用页面

--- miniprogram/pages/search/cst.js
+++
@@ -1,7 +0,0 @@
-export const URL = 'https://appo4.owhat.cn/api';
-export const HEADER_GOODINTRO = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Accept': 'application/json',
-  'Accept-Language': 'zh-CN,zh;q=0.9',
-};
-export const CLIENT_INFO = '{"platform":"mobile","version":"1.5.6L","deviceid":"413aca0f-a134-3d02-b7ed-1326c5cb0543","channel":"owhat","deviceIdmd5":"085740aa14161d3f642c9b2c90f3d804","deviceIdmd5salt":"38d6b875ddcc045d26b5e8017bca512a","timesalt":1595968533698}';
